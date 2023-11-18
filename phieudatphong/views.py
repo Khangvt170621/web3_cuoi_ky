ebc0333c5a7bf55ff4193edb8a7f29c50efaa18d
Khangvt170621 <to29a3vt@gmail.com>
2023-11-18 22:53:19 +0700

    final all  commit

--- phieudatphong/views.py
+++ phieudatphong/views.py
@@ -4,26 +4,31 @@ from .forms import DatPhongForm
 from .models import PhieuDatPhong
 
 def dat_phong(request):
-    ma_phong = request.GET.get('id')
-    print(ma_phong)
-    if not ma_phong:
-        # Xử lý trường hợp không có giá trị MaPhong được truyền vào
-        return redirect('index')  # Chẳng hạn, chuyển hướng về trang chủ
+    # ma_phong = request.POST.get(form.MaPhong)
+    # print(ma_phong)
+    # if not ma_phong:
+    #     # Xử lý trường hợp không có giá trị MaPhong được truyền vào
+    #     return redirect('index')  # Chẳng hạn, chuyển hướng về trang chủ
 
     # Lấy thông tin người dùng đăng nhập và mã khách hàng
     if request.user.is_authenticated:
+
         ma_khach_hang = request.user.khachhang.Cccd
     else:
         # Xử lý trường hợp người dùng không đăng nhập
         return redirect('login')  # Chẳng hạn, chuyển hướng đến trang đăng nhập
 
-    if request.method == 'post':
+    if request.method == 'POST':
+        print("Đã vào post")
         form = DatPhongForm(request.POST)
         if form.is_valid():
+            print("Trong valid:", form.fields['MaPhong'])
             phieu_dat_phong = form.save(commit=False)
             phieu_dat_phong.save()
             return redirect('phieudat')
     else:
+        ma_phong = request.GET.get('id')
+        print('Ngoai POST:', ma_phong)
         form = DatPhongForm(initial={'MaPhong': ma_phong, 'MaKH': ma_khach_hang})
 
     return render(request, 'datphong/datphong.html', {'form': form})
